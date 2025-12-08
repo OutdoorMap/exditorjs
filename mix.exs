357228c6829dba5c299a32ec4c04efec8665bc66
Joakim Nylen <git@joakim.nylen.nu>
2025-12-08 15:10:16 +0100

    fixes

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule ExditorJS.MixProject do
   use Mix.Project
 
+  @version "0.1.3"
+
   def project do
     [
       app: :exditorjs,
-      version: "0.1.2",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
