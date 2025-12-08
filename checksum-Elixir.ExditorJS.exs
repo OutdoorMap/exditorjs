685a6ea0298b083303a652c26f75c06853c166e5
Joakim Nylen <git@joakim.nylen.nu>
2025-12-08 15:19:36 +0100

    fix: add checksum

---
+++ checksum-Elixir.ExditorJS.exs
@@ -0,0 +1,12 @@
+%{
+  "exditorjs_native-v0.1.4-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:ad83a4c8d1b3a24e2d1aabb6e963d74e5718f95ef053b191c2e2f188b06a1f86",
+  "exditorjs_native-v0.1.4-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5bbee8c364774f90ff6e6a19b8b3fba9c36212f44747e2664164329359f931da",
+  "libexditorjs_native-v0.1.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:269ab773b1b10b16228fbfe4422d7554c763f9f6765f190173cefaaf394b7825",
+  "libexditorjs_native-v0.1.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:487842e0ee9bc65e7a295cbb88f033b95ef44949d3c32c391396504dc4267f9c",
+  "libexditorjs_native-v0.1.4-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:a7fa428a9ae08b1d7e1d36be5ef998246b8c114de0fc7629ad134a91bdaa2e0d",
+  "libexditorjs_native-v0.1.4-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:b30f7216520f7c3e1785131df7a92830cae48b6114497904d83e2c2b3bb85dd2",
+  "libexditorjs_native-v0.1.4-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:fc512b82bf973c8dcb567aa1e09c3a66d10ec6e901f77b04e4ee5d5157ae5e4a",
+  "libexditorjs_native-v0.1.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:86337698cd68329280389e5d19ec9727fcca705ef95c43f11ad8a70f73995a79",
+  "libexditorjs_native-v0.1.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:53d5b69bb52d1206d5d9a60450683f9a23fc5c618ce93fece9c6e52dcf38f4b4",
+  "libexditorjs_native-v0.1.4-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:80281a3507f02b7f456e18241efa77e3f2ba88c549fc8079e9d8c8d653e6d55b",
+}
